4ddae5515c0fed3bc91339e1738769facf3a3b11
britoalana <110117693+britoalana@users.noreply.github.com>
2023-04-17 17:14:14 -0300

    Add files via upload

---
+++ objeto/objeto.js
@@ -0,0 +1,22 @@
+//cóigo não executável
+
+//precedural
+
+processamento(valor1, valor2, valor3)
+
+//orientação ao objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processamento() { }
+}
+
+objeto = ['valor1', 'valor2', 'valor3']
+
+//principios da orientação ao objeto
+//1 - abstração - colocar caracteristicas do mundo real no sistema
+//2 - encapsulamento - ligar carro e não saber o que acontece no motor para ele funcionar, preservar códigos dentro do sistema sem o usuario saber o que está acontecendo
+//3 - herança(prototype) - elemento filho adquire caracteristicas do elemento pai
+//4 - polimorfismo - tornar 
